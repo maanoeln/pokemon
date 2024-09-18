0deac5176f35651ad5b08854ef2acfe59fd7e5a5
Manoel Neto <manoel.neto@libercapital.com.br>
2024-09-18 07:58:42 -0300

    feat: create router componet

---
+++ src/router/index.tsx
@@ -0,0 +1,23 @@
+import App from '@/App';
+import PokemonInfoByName from '@/pages/PokemonInfoByName';
+import PokemonListPage from '@/pages/PokemonList';
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        index: true,
+        element: <PokemonListPage />,
+      },
+      {
+        path: '/pokemon/:name',
+        element: <PokemonInfoByName />,
+      },
+    ],
+  },
+]);
+
+export default router;
