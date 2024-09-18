f2f9c6dede9964ef6b13f61cc22ea79fcdaf2d9d
Manoel Neto <manoel.neto@libercapital.com.br>
2024-09-18 15:49:34 -0300

    test: implement unit test for footer

---
+++ src/components/Footer/index.test.tsx
@@ -0,0 +1,27 @@
+import { dark } from '@/css/dark.ts';
+import '@testing-library/jest-dom';
+import FooterComponent from './index.tsx';
+import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+
+const mockWithTheme = () => {
+  return (
+    <ThemeProvider theme={dark}>
+      <FooterComponent />
+    </ThemeProvider>
+  );
+};
+describe('Footer Componet', () => {
+  it('Should render successfully with all icons', () => {
+    const { container } = render(mockWithTheme());
+
+    expect(container.querySelectorAll('svg')).toHaveLength(3);
+  });
+
+  it('Should render successfully with developers name', () => {
+    render(mockWithTheme());
+
+    expect(screen.getByText(/Manoel Neto/i)).toBeDefined();
+    expect(screen.getByText(/Manoel Neto/i)).toBeInTheDocument();
+  });
+});
