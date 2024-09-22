337e2f1f17102bafafca08651116c0c0b397db0a
Manoel Neto <manoel.neto@libercapital.com.br>
2024-09-22 15:13:52 -0300

    feat: create icons component

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,34 @@
+import icons from '@/components/Icons/iconsObject';
+import { IconDiv } from '@/components/Icons/styles';
+
+interface IProps {
+  name: string;
+  viewBox?: string;
+  height?: string;
+  width?: string;
+  onClick?(): void;
+}
+
+function Icons({
+  name,
+  viewBox = '0 0 16 16',
+  width = '',
+  height = '',
+  onClick,
+}: IProps) {
+  return (
+    <IconDiv onClick={onClick} data-testid={name}>
+      <svg
+        style={{ display: `flex`, flex: 1 }}
+        viewBox={viewBox}
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+      >
+        <path d={icons[name]} />
+      </svg>
+    </IconDiv>
+  );
+}
+
+export default Icons;
