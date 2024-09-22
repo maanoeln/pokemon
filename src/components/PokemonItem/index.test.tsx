96376a91b9344e1a610acf3b99dd60d481b684ce
Manoel Neto <manoel.neto@libercapital.com.br>
2024-09-22 15:28:16 -0300

    test: refactor test for pokemon item

--- src/components/PokemonItem/index.test.tsx
+++ src/components/PokemonItem/index.test.tsx
@@ -1,30 +1,54 @@
 import PokemonItemComponent from '@/components/PokemonItem';
-import '@testing-library/jest-dom';
-import { dark } from '@/css/dark';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { ThemeProvider } from 'styled-components';
+import * as helpers from '@/helpers/handleFavoritePokemons';
+import RenderMockedComponent from '../../../mocks/createComponentWithStore';
 
 const mockOnClick = jest.fn();
 
-const mockWithTheme = () => {
+const pokemonMock = {
+  id: 1,
+  name: 'bulbassaur',
+};
+
+const returnComponent = () => {
   return (
-    <ThemeProvider theme={dark}>
-      <PokemonItemComponent id={1} name="bulbassaur" onClick={mockOnClick} />
-    </ThemeProvider>
+    <RenderMockedComponent>
+      <PokemonItemComponent {...pokemonMock} onClick={mockOnClick} />
+    </RenderMockedComponent>
   );
 };
 
 describe('PokemonItem', () => {
   it('Should render component sucessfully', async () => {
-    render(mockWithTheme());
+    render(returnComponent());
 
     expect(screen.getByText(/#0001/i)).toBeInTheDocument();
     expect(screen.getByTestId('bulbassaur')).toBeInTheDocument();
-    expect(screen.getByText('Favoritar')).toBeInTheDocument();
     expect(screen.getByText('+ Detalhes')).toBeInTheDocument();
 
     await userEvent.click(screen.getByText('+ Detalhes'));
-    expect(mockOnClick).toHaveBeenCalledWith('bulbassaur');
+    expect(mockOnClick).toHaveBeenCalledWith(1, 'bulbassaur');
+  });
+
+  it('Should render heart not filled sucessfully', async () => {
+    render(returnComponent());
+
+    expect(screen.getByTestId('EmptyHeart')).toBeInTheDocument();
+  });
+
+  it('When click on heart should call function and change image to filled heart', async () => {
+    const spy = jest.spyOn(helpers, 'handleFavorites');
+
+    render(returnComponent());
+
+    const emptyHeart = screen.getByTestId('EmptyHeart');
+    await userEvent.click(emptyHeart);
+
+    await waitFor(() => {
+      expect(spy).toHaveBeenCalled();
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(screen.getByTestId('FilledHeart')).toBeInTheDocument();
+    });
   });
 });
