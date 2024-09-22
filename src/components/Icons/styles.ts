3e1d3b8a6a1d174d71425a19fee4153e38b3c508
Manoel Neto <manoel.neto@libercapital.com.br>
2024-09-22 15:14:23 -0300

    style: create icons component styles

---
+++ src/components/Icons/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const IconDiv = styled.div`
+  display: flex;
+  cursor: pointer;
+
+  svg {
+    fill: #e2e2e2;
+
+    &:hover {
+      fill: red;
+    }
+  }
+`;
