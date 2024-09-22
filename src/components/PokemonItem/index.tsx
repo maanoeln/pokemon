27d3647cc7f7b0236719e5c1832a11dd446e4b05
Manoel Neto <manoel.neto@libercapital.com.br>
2024-09-22 15:09:48 -0300

    refactor: refactor pokem item component

--- src/components/PokemonItem/index.tsx
+++ src/components/PokemonItem/index.tsx
@@ -1,23 +1,42 @@
+import ButtonComponent from '@/components/Button';
+import Icons from '@/components/Icons';
 import {
-  Button,
   ButtonContainer,
   PokemonCard,
   PokemonId,
   PokemonName,
 } from '@/components/PokemonItem/styles';
 import { capitalizeFirstLetter } from '@/helpers/capitalizeFirstLetter';
+import { isPokemonFavorite } from '@/helpers/getFavoritesPokemos';
+import { handleFavorites } from '@/helpers/handleFavoritePokemons';
+import { returnFavoriteIconConfigs } from '@/helpers/returnFavoriteIconConfigs';
 import { formattedPokemnonId } from '@/helpers/returnFormattedPokemId';
+import { AppDispatch, RootState } from '@/store/store';
+import { useDispatch, useSelector } from 'react-redux';
 
 interface IProps {
   id: number;
   name: string;
-  onClick(value: string): void;
+  onClick(id: number, name: string): void;
 }
 
 function PokemonItemComponent({ id, name, onClick }: IProps) {
+  const pokemons = useSelector((state: RootState) => state.pokemons);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const isFavorite = isPokemonFavorite(pokemons, id);
+
   return (
     <PokemonCard>
-      <PokemonId>{`#${formattedPokemnonId(id.toString())}`}</PokemonId>
+      <PokemonId>
+        {`#${formattedPokemnonId(id.toString())}`}{' '}
+        <Icons
+          {...returnFavoriteIconConfigs(isFavorite)}
+          width="25px"
+          height="25px"
+          onClick={() => handleFavorites(+id!, `${name}`, isFavorite, dispatch)}
+        />
+      </PokemonId>
       <img
         src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${id}.svg`}
         alt={name}
@@ -28,10 +47,9 @@ function PokemonItemComponent({ id, name, onClick }: IProps) {
       <PokemonName>{capitalizeFirstLetter(name)}</PokemonName>
 
       <ButtonContainer>
-        <Button primary={false}>Favoritar</Button>
-        <Button primary onClick={() => onClick(name)}>
+        <ButtonComponent primary onClick={() => onClick(id, name)}>
           + Detalhes
-        </Button>
+        </ButtonComponent>
       </ButtonContainer>
     </PokemonCard>
   );
