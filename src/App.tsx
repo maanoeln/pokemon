2636ef46c94596b19350177fcfc04d3a0806b577
Manoel Neto <manoel.neto@libercapital.com.br>
2024-09-22 15:23:23 -0300

    feat: add toaster component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import HeaderComponent from '@/components/Header';
 import WrapperComponent from '@/components/Wrapper';
 import GlobalStyle from '@/globalStyles';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <HeaderComponent />
       <Outlet />
       <FooterComponent />
+      <ToastContainer />
     </WrapperComponent>
   );
 }
