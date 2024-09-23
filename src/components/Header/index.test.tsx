47a1581d2d1e7c446cbd9144b6532b5e41e378ab
Manoel Neto <manoel.neto@libercapital.com.br>
2024-09-23 00:37:46 -0300

    test: implement new header test

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,21 +1,34 @@
 import HeaderComponent from '@/components/Header';
 import RenderMockedComponent from '../../../mocks/createComponentWithStore';
 import { render, screen } from '@testing-library/react';
+import { PokemonState } from '@/store/pokemonSlice';
 
-const returnComponent = () => {
+const pokemon = [{ id: 1, name: 'bulbassaur' }];
+
+const returnComponent = (pokemons?: PokemonState[]) => {
   return (
-    <RenderMockedComponent>
+    <RenderMockedComponent pokemons={pokemons}>
       <HeaderComponent />
     </RenderMockedComponent>
   );
 };
 
 describe('Header', () => {
-  it('ensure header componenet renders successfully', () => {
+  it('ensure header componenet renders successfully and does not have a favorite pokemon', () => {
     render(returnComponent());
 
     expect(screen.getByTestId('logo')).toBeInTheDocument();
     expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getByText('Favoritos')).toBeInTheDocument();
     expect(screen.getByRole('button')).toBeDisabled();
+  });
+
+  it('ensure header componenet renders successfully and does have a favorite pokemon text should change', () => {
+    render(returnComponent(pokemon));
+
+    expect(screen.getByTestId('logo')).toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getByText('Favoritos (1)')).toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeEnabled();
   });
 });
