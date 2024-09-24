5b13a217ab6bc3cbf040f6b56b7908efc26ea48c
Manoel Neto <manoel.neto@libercapital.com.br>
2024-09-24 20:54:33 -0300

    style: add style for scroolbar

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -7,6 +7,29 @@ const GlobalStyle = createGlobalStyle`
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     -o-box-sizing: border-box;
+
+    ::-webkit-scrollbar {
+      border-radius: 10px;
+      width: 3px;
+      background: ${({ theme }) => theme.colors.cardTitle};
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+      box-shadow: inset 0 0 5px ${({ theme }) => theme.colors.cardBg};
+      border-radius: 10px;
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+      background: ${({ theme }) => theme.colors.cardBg};
+      border-radius: 10px;
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+      background: ${({ theme }) => theme.colors.cardBg};
+    }
   }
 
   html {
