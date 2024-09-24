8f989a29cc6db1ad7b95180f63ab1e0e7f1c9adb
Manoel Neto <manoel.neto@libercapital.com.br>
2024-09-24 20:55:40 -0300

    style: create wrapper

--- src/pages/PokemonList/styles.ts
+++ src/pages/PokemonList/styles.ts
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  height: 100%;
+  padding: 1.6em 0;
+  overflow: hidden;
+`;
+
 export const ListWrapper = styled.div`
-  padding: 16px;
+  margin-right: 0.4em;
+  padding: 0 1.6em 0 1.2em;
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-template-rows: repeat(4, 1fr);
   gap: 16px;
-  overflow: hidden;
   overflow-y: scroll;
   height: 100%;
 `;
