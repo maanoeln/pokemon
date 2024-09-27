4064975a211d8fd2f92037e64a7ffc6b881c2326
Manoel Neto <mneto_004@hotmail.com>
2024-09-27 10:18:52 -0300

    test: add mock in setup tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,9 @@
 import '@testing-library/jest-dom';
 import 'jest-styled-components';
+
+export const mockNavigate = jest.fn();
+
+jest.mock(`react-router-dom`, () => ({
+  ...jest.requireActual(`react-router-dom`),
+  useNavigate: () => mockNavigate,
+}));
