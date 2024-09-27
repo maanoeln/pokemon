b63757de4c328b7aa9e9d5c446a547ae989ee203
Manoel Neto <mneto_004@hotmail.com>
2024-09-27 07:30:33 -0300

    feat: create pokemon specs buttons

---
+++ src/components/PokemonSpecsButton/index.tsx
@@ -0,0 +1,55 @@
+import { ButtonsContainer } from '@/components/PokemonSpecsButton/style';
+import { ArrowBack, ArrowForward, Home } from '@mui/icons-material';
+import { Button } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+
+interface IProps {
+  id: string;
+}
+
+function PokemonSpecsButtons({ id }: IProps) {
+  const navigate = useNavigate();
+  const { t } = useTranslation('flex');
+  const isButtonDisabled = !!id && +id === 1;
+
+  const handleNavigate = (id?: string) => {
+    const url = id ? `/pokemon/${id}` : `/`;
+    navigate(url);
+  };
+
+  return (
+    <ButtonsContainer>
+      <Button
+        startIcon={<ArrowBack />}
+        variant="contained"
+        size="medium"
+        onClick={() => handleNavigate(`${+id! - 1}`)}
+        disabled={isButtonDisabled}
+      >
+        {t('previous')}
+      </Button>
+
+      <Button
+        startIcon={<Home />}
+        variant="outlined"
+        size="medium"
+        onClick={() => handleNavigate()}
+        color="secondary"
+      >
+        {t('go_back_homepage')}
+      </Button>
+
+      <Button
+        endIcon={<ArrowForward />}
+        variant="contained"
+        size="medium"
+        onClick={() => handleNavigate(`${+id! + 1}`)}
+      >
+        {t('next')}
+      </Button>
+    </ButtonsContainer>
+  );
+}
+
+export default PokemonSpecsButtons;
