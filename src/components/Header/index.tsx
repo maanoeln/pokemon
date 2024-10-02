8b20fc3affb733b94b91ed78b806bef8776ed8d9
Manoel Neto <mneto_004@hotmail.com>
2024-10-01 23:01:22 -0300

    feat: add navigation component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,42 +1,24 @@
-import {
-  FavoriteButton,
-  FlexDiv,
-  Header,
-  HeaderItems,
-} from '@/components/Header/styles';
+import { FlexDiv, Header, HeaderItems } from '@/components/Header/styles';
 import pokemon from '@/assets/img/pokemon.png';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/store/store';
-import { Badge } from '@mui/material';
 import MenuComponent from '@/components/Menu';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import NavigationComponent from '@/components/Navigation';
 
 function HeaderComponent() {
-  const pokemons = useSelector(
-    (state: RootState) => state.pokemons.favoritePokemons,
-  );
+  const navigate = useNavigate();
   const { t } = useTranslation('flex');
 
   return (
     <Header>
       <HeaderItems>
-        <img src={pokemon} alt="Pokemon" width="150" data-testid="logo" />
-
         <FlexDiv>
-          {!!pokemons.length && (
-            <Badge
-              color="primary"
-              badgeContent={pokemons.length}
-              data-testid="badge"
-            >
-              <FavoriteButton>
-                {t('favorites', { count: pokemons.length })}
-              </FavoriteButton>
-            </Badge>
-          )}
+          <img src={pokemon} alt="Pokemon" width="150" data-testid="logo" />
 
-          <MenuComponent />
+          <NavigationComponent t={t} navigate={navigate} />
         </FlexDiv>
+
+        <MenuComponent />
       </HeaderItems>
     </Header>
   );
