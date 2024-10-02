8f205c92077050399892f45d9d5bcb5cfefa53c7
Manoel Neto <mneto_004@hotmail.com>
2024-10-01 22:54:23 -0300

    refactor: remove unecessary fn

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://pokeapi.co/api/v2/',
 });
+
+export const getPokemons = async () => {
+  return api.get('/pokemon').then((res) => res);
+};
