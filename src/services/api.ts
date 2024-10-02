e3f7237042af598c978d919be34638bd4e5f86a6
Manoel Neto <mneto_004@hotmail.com>
2024-10-02 04:55:54 -0300

    test: remove api gn that was not used

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,3 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://pokeapi.co/api/v2/',
 });
-
-export const getPokemons = async () => {
-  return api.get('/pokemon').then((res) => res);
-};
