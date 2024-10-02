3cf997fad64a516a40e2f2d6b43afee557b4225d
Manoel Neto <mneto_004@hotmail.com>
2024-10-01 22:59:53 -0300

    feat: add mock to useParams

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,8 +2,10 @@ import '@testing-library/jest-dom';
 import 'jest-styled-components';
 
 export const mockNavigate = jest.fn();
+export const mockParams = jest.fn();
 
 jest.mock(`react-router-dom`, () => ({
   ...jest.requireActual(`react-router-dom`),
   useNavigate: () => mockNavigate,
+  useParams: mockParams,
 }));
